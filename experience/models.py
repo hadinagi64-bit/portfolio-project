064a39130f2cfaf20b27010c8018173d6ff87d3a
hadinagi64-bit <hadinagi64@gmail.com>
2026-06-26 09:51:48 -0700

    Portfolio project completed

---
+++ experience/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ portfolio/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from bio.models import Bio
+from education.models import Education
+from skills.models import Skill
+from experience.models import Experience
+from projects.models import Project
+
+
+def home(request):
+    bio = Bio.objects.first()
+    education = Education.objects.all()
+    skills = Skill.objects.all()
+    experiences = Experience.objects.all()
+    projects = Project.objects.all()
+
+    context = {
+        'bio': bio,
+        'education': education,
+        'skills': skills,
+        'experiences': experiences,
+        'projects': projects,
+    }
+
+    return render(request, 'home.html', context)
